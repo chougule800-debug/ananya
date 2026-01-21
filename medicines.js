b585c1322a34d81a01fc955551e52806e28dbb43
prajwal07-pro <prajwalprawoot@gmail.com>
2026-01-21 21:21:17 +0530

    first commit

---
+++ medicines.js
@@ -0,0 +1,21 @@
+// medicines.js
+// You can easily add more medicines here.
+// Format: "Medicine Name": ["Strength 1", "Strength 2", "Type"]
+
+const medicineDatabase = {
+    "Paracetamol": ["500 mg", "650 mg", "Syrup", "Injection"],
+    "Gabapentin": ["100 mg", "300 mg", "400 mg", "800 mg"],
+    "Pregabalin": ["50 mg", "75 mg", "150 mg", "300 mg"],
+    "Metformin": ["500 mg", "850 mg", "1000 mg", "SR 500mg"],
+    "Atorvastatin": ["10 mg", "20 mg", "40 mg", "80 mg"],
+    "Levothyroxine": ["25 mcg", "50 mcg", "75 mcg", "88 mcg", "100 mcg"],
+    "Amitriptyline": ["10 mg", "25 mg", "50 mg", "75 mg"],
+    "Clopidogrel": ["75 mg", "150 mg", "300 mg"],
+    "Pantoprazole": ["20 mg", "40 mg", "Injection", "IV"],
+    "Vitamin B12": ["Tablet", "Injection", "Syrup"],
+    "Aspirin": ["75 mg", "81 mg", "150 mg", "325 mg"],
+    "Amoxicillin": ["250 mg", "500 mg", "Syrup 125mg", "Syrup 250mg"],
+    "Omeprazole": ["10 mg", "20 mg", "40 mg"],
+    "Ibuprofen": ["200 mg", "400 mg", "600 mg", "Syrup"],
+    "Cetirizine": ["5 mg", "10 mg", "Syrup"]
+};
